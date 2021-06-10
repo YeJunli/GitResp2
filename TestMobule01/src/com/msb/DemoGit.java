f9831ff18a5caae2a22b740f4c54bf446c2420d3
yejunlichn <yejunlichn@aliyun.com>
2021-06-10 18:05:12 +0800

    修改了代码

--- TestMobule01/src/com/msb/DemoGit.java
+++ TestMobule01/src/com/msb/DemoGit.java
@@ -10,4 +10,37 @@ public class DemoGit {
     private String mame;
     private int age;
     private String className;
+
+    public String getMame() {
+        return mame;
+    }
+
+    public void setMame(String mame) {
+        this.mame = mame;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public DemoGit() {
+    }
+
+    public DemoGit(String mame, int age, String className) {
+        this.mame = mame;
+        this.age = age;
+        this.className = className;
+    }
 }
