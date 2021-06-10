ea688804b20130d0979ea8d0156076e496d8a9e4
yejunlichn <yejunlichn@aliyun.com>
2021-06-10 18:40:42 +0800

    创建了一个Person类

---
+++ TestMobule01/src/com/msb/Person.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: YeZhanCHN
+ * @Date: 2021/6/10 - 06 - 10 - 18:35}
+ * @Description: 创建一个Persion类
+ * @version:1.0
+ */
+public class Person {
+    String name;
+    String sex;
+}
